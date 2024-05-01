fba3f8e0077257ffef8297d61ec4b0d74050b29e
Carolina Giacchi <carogiacchi@gmail.com>
2024-04-30 21:43:11 -0300

    desafio4

--- Desafios/desafio4/src/app.js
+++ Desafios/desafio4/src/app.js
@@ -1,27 +1,61 @@
-import express from 'express';
-import handlebars from 'express-handlebars';
-import __dirname from './utils.js';
-
+import express from "express";
+import { ProductManager } from "./managers/productManager.js";
+import { CartManager } from "./managers/cartManager.js";
+import { productsRouter } from "./routes/products.router.js";
+import { cartsRouter } from "./routes/carts.router.js";
+import expressHandlebars from "express-handlebars";
+import { routerHome } from "./routes/views.router.js";
+import { Server } from "socket.io";
+import path from "path";
+import fs from "fs";
 const app = express();
+const productsPath = './src/data/products.json';
+
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import http from "http";
+const server = http.createServer(app);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const PORT = 8080;
 
+app.use(express.static(__dirname + "/public"));
+app.engine("handlebars", expressHandlebars.engine()); 
+app.set("view engine", "handlebars"); 
+app.set("views", __dirname + "/views"); 
+
+export const productManager = new ProductManager();
+export const cartManager = new CartManager();
+
+fs.readFile(productsPath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Error al leer el archivo:', err);
+        return;
+    }
+    const productos = JSON.parse(data);
+
+    app.use((req, res, next) => {
+        res.locals.productos = productos;
+        next();
+    });
+});
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
-app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views')
-app.set('view engine', 'handlebars')
-app.use(express.static(__dirname+ '/public'))
+app.use("/views", routerHome);
 
-app.get('/', (req,res)=>{
-    let user ={
-        nombre : "coder",
-        edad : 25
-    }
-    res.render('index', user)
-})
+const io = new Server(server);
+io.on("connection", async (socket) => {
+    console.log("User conectado");
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    const products = await productManager.getProducts();
+    socket.emit("products", products);
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor en el puerto ${PORT}`);
 });
---
+++ Desafios/desafio4/src/routes/views.router.js
@@ -0,0 +1,13 @@
+import express from "express";
+const { Router } = express;
+const router = new Router();
+
+router.get("/home", (req, res) => {
+  res.render("home", {});
+});
+
+router.get("/realtimeproducts", (req, res) => {
+  res.render("realTimeProducts", {});
+});
+
+export { router as routerHome };
